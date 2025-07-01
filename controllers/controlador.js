6ca0946f563805884fae3d236bdbf052ea0bfdae
207 <207@academicos.cesde.edu.co>
2025-07-01 17:22:38 -0500

    cambios

---
+++ controllers/controlador.js
@@ -0,0 +1,24 @@
+let boton = document.getElementById("boton")
+let nombres = document.getElementById("nombres")
+let salario = document.getElementById("salario")
+let horas = document.getElementById("horas")
+let cargo = document.getElementById("cargo")
+let experiencia = document.getElementById("experiencia")
+
+//Escuchamos el evento de hacer click al botón del formulario
+boton.addEventListener("click", function(evento){
+    evento.preventDefault()
+    nombreIngresado = nombres.value
+    salarioIngresado = salario.value
+    horasIngresadas = horas.value
+    cargoIngresado = cargo.value
+    experienciaIngresada = experiencia.value
+
+    let datos = JSON.stringify({
+        nombresEmpleado: nombreIngresado,
+        salarioEmpleado: salarioIngresado,
+        horasEmpleado: horasIngresadas,
+        cargoEmpleado: cargoIngresado,
+        experienciaEmpleado: experienciaIngresada
+    })
+})
